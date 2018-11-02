e94ad06828c09a181962d6c486c8f4c9b3ee94f6
Alexis Pequeño <alxpequeno@gmail.com>
2018-11-02 01:55:00 -0500

    registrar empleados y navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { RegistrarEmpleadoComponent } from './components/registrar-empleado/registrar-empleado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    RegistrarEmpleadoComponent
   ],
   imports: [
     BrowserModule
